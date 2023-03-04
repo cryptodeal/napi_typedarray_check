ecd302a45af75b89234a3c498d4bbd1b1ea9edc0
cryptodeal <39316528+cryptodeal@users.noreply.github.com>
2023-03-03 20:30:55 -0600

    clarify what causes failure

--- index.cjs
+++ index.cjs
@@ -4,10 +4,6 @@ CheckType(new Float32Array([100]))
 
 CheckType(new Float64Array([100]))
 
-CheckType(new BigInt64Array([BigInt(1)]))
-
-CheckType(new BigUint64Array([BigInt(1)]))
-
 CheckType(new Int8Array([1]))
 
 CheckType(new Int16Array([1]))
@@ -19,4 +15,9 @@ CheckType(new Uint8Array([1]))
 CheckType(new Uint16Array([1]))
 
 CheckType(new Uint32Array([1]))
+
+// `BigInt64Array` & `BigUint64Array` both cause native code to throw `TypeError`
+CheckType(new BigInt64Array([BigInt(1)]))
+
+CheckType(new BigUint64Array([BigInt(1)]))
 
